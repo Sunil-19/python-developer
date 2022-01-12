b6e61dad64c08e137ff4a6d7e914b023580bad14
Sunil-19 <97602357+Sunil-19@users.noreply.github.com>
2022-01-12 20:34:49 +0530

    Update and rename listuniqdictionary.py to 6.listuniqdictionary.py

--- 6.listuniqdictionary.py
+++ 6.listuniqdictionary.py
@@ -4,11 +4,11 @@ l=[{'name': 'affirm', 'confidence': 0.9448149204254},
    {'name': 'inform', 'confidence': 0.9842240810394287}]
 
 seen = set()
-new_l = []
+new = []
 for d in l:
     t = tuple(d.items())
     if t not in seen:
         seen.add(t)
         new_l.append(d)
 
-print (new_l)
+print (new)
